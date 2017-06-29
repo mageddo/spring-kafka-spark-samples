741c2b68864e807d90e30a76dec680c83bb1942a
Elvis de Freitas <edigitalb@gmail.com>
2017-06-28 23:57:19 -0300

    MG-311 fixing test

--- kafka-spring/src/test/java/com/mageddo/kafka/AutoCommitTest.java
+++ kafka-spring/src/test/java/com/mageddo/kafka/AutoCommitTest.java
@@ -7,11 +7,13 @@ import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +23,7 @@ import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.test.context.ContextConfiguration;
@@ -28,10 +31,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static com.mageddo.kafka.Application.SERVER;
+import static com.mageddo.kafka.config.AutoCommitConfig.PING_TEMPLATE;
 
 /**
  * Created by elvis on 26/06/17.
@@ -43,11 +49,12 @@ import static com.mageddo.kafka.Application.SERVER;
 public class AutoCommitTest {
 
 	private static final String PING = "Ping";
-	private static final String PING_TEMPLATE = "PingTemplate";
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private CountDownLatch countDownLatch = new CountDownLatch(4);
 
 	@Autowired
+	@Qualifier(PING_TEMPLATE)
 	private KafkaTemplate<Integer, String> template;
 
 	@Test
@@ -55,69 +62,22 @@ public class AutoCommitTest {
 
 		this.template.send(PING, new Integer(1), "foo1");
 		this.template.send(PING, new Integer(2), "foo2");
-		this.template.send(PING, new Integer(1),"foo3");
+		this.template.send(PING, new Integer(3), "foo3");
+		this.template.send(PING, new Integer(4), "foo4");
 
-
+		Assert.assertTrue(countDownLatch.await(10, TimeUnit.SECONDS));
 
 	}
 
-	@KafkaListener( topicPartitions = {
-			@TopicPartition(topic = PING, partitions = {"0", "1", "2"},
-				partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "0"))
-		})
+	@KafkaListener( /*topicPartitions = {
+			@TopicPartition(topic = PING, partitions = {"0", "1", "2", "3"})
+				//partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "0"))
+		}*/ topics = PING)
 	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
 		logger.info("status=received, msg={}", cr.toString());
+		countDownLatch.countDown();
 	}
 
-	@Bean
-	ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory() {
-		ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
-			new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(consumerFactory());
-		factory.setConcurrency(3);
-		return factory;
-	}
 
-	@Bean
-	public ConsumerFactory<Integer, String> consumerFactory() {
-		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-	}
-
-	@Bean
-	public Map<String, Object> consumerConfigs() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVER);
-		props.put(ConsumerConfig.GROUP_ID_CONFIG, "group.a");
-		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
-		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
-		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
-		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		return props;
-	}
-
-	@Bean
-	public ProducerFactory<Integer, String> producerFactory() {
-		return new DefaultKafkaProducerFactory<>(producerConfigs());
-	}
-
-	@Bean
-	public Map<String, Object> producerConfigs() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVER);
-		props.put(ProducerConfig.RETRIES_CONFIG, 0);
-		props.put(ProducerConfig.RETRIES_CONFIG, 0);
-		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		return props;
-	}
-
-	@Bean(PING_TEMPLATE)
-	public KafkaTemplate<Integer, String> kafkaTemplate() {
-		return new KafkaTemplate<>(producerFactory());
-	}
 
 }
