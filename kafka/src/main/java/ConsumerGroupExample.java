25ca0beaca0ea0f7f1c026e2e1242f4e38102590
Elvis de Freitas <edigitalb@gmail.com>
2017-06-30 02:48:13 -0300

    MG-311 creating producer

--- kafka/src/main/java/ConsumerGroupExample.java
+++ kafka/src/main/java/ConsumerGroupExample.java
@@ -1,9 +1,18 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import kafka.admin.AdminUtils;
+
+import kafka.admin.RackAwareMode;
+import kafka.utils.ZKStringSerializer$;
+import kafka.utils.ZkUtils;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkConnection;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -12,11 +21,13 @@ public class ConsumerGroupExample {
     private String a_groupId;
     private final String topic;
     private ExecutorService executor;
+    private String kafkaServer;
 
-    public ConsumerGroupExample(String a_zookeeper, String a_groupId, String a_topic) {
+    public ConsumerGroupExample(String a_zookeeper, String a_groupId, String a_topic, String kafkaServer) {
         this.a_zookeeper = a_zookeeper;
         this.a_groupId = a_groupId;
         this.topic = a_topic;
+        this.kafkaServer = kafkaServer;
     }
  
     public void shutdown() {
@@ -40,17 +51,18 @@ public class ConsumerGroupExample {
         int threadNumber = 0;
         for (int i = 0; i < a_numThreads; i++) {
             final KafkaConsumer<String, Object> consumer = new KafkaConsumer<>(
-                    ConsumerGroupExample.createConsumerConfig(a_zookeeper, a_groupId));
+                    ConsumerGroupExample.createConsumerConfig(a_zookeeper, a_groupId, kafkaServer));
             consumer.subscribe(Arrays.asList(this.topic));
+//            consumer.seekToBeginning(consumer.assignment());
             executor.submit(new ConsumerTest(consumer, threadNumber));
             threadNumber++;
 
         }
     }
  
-    private static Properties createConsumerConfig(String a_zookeeper, String a_groupId) {
+    private static Properties createConsumerConfig(String a_zookeeper, String a_groupId, String kafkaServer) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "kafka.dev:9092");
+        props.put("bootstrap.servers", kafkaServer);
         props.put("zookeeper.connect", a_zookeeper);
         props.put("group.id", a_groupId);
         props.put("zookeeper.session.timeout.ms", "400");
@@ -66,16 +78,44 @@ public class ConsumerGroupExample {
         return props;
     }
  
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, InterruptedException {
 
         String zooKeeper = "zookeeper.dev:2181";
+        String kafka =  "kafka.dev:9092";
         String groupId = "ping.a";
         String topic = "Ping";
-        int threads = 2;
- 
-        ConsumerGroupExample example = new ConsumerGroupExample(zooKeeper, groupId, topic);
+        int threads = 20;
+
+        // Topic creation
+        //
+        final int portIndex = zooKeeper.indexOf(":");
+        String address = zooKeeper.substring(0, portIndex);
+        final String completeAddress = InetAddress.getByName(address).getHostAddress() + zooKeeper.substring(portIndex, zooKeeper.length());
+
+        ZkClient client = new ZkClient(completeAddress, 10000, 10000, ZKStringSerializer$.MODULE$);
+        ZkUtils zkUtils = new ZkUtils(client, new ZkConnection(completeAddress), false);
+
+//        if(AdminUtils.topicExists(zkUtils, topic)){
+//            AdminUtils.deleteTopic(zkUtils, topic);
+//            AdminUtils.deleteAllConsumerGroupInfoForTopicInZK(zkUtils, topic);
+////            AdminUtils.deleteConsumerGroupInZK(zkUtils, groupId);
+//        }
+//        try{
+//            AdminUtils.createTopic(zkUtils, topic, 10, 1, new Properties(), RackAwareMode.Disabled$.MODULE$);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+
+        // Consumption creation
+        //
+        ConsumerGroupExample example = new ConsumerGroupExample(zooKeeper, groupId, topic, kafka);
         example.run(threads);
- 
+
+        Thread.sleep(3000);
+        // Producer creation
+        //
+        new ProducerExample(kafka, topic);
+
         try {
             Thread.sleep(1000000);
         } catch (InterruptedException ie) {
---
+++ kafka/src/main/java/ProducerExample.java
@@ -0,0 +1,46 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by elvis on 30/06/17.
+ */
+public class ProducerExample {
+
+	public ProducerExample(String kafka, String topic) throws InterruptedException {
+		final KafkaProducer<String, Object> producer = new KafkaProducer<>(producerConfigs(kafka));
+
+		while(true){
+
+			final ProducerRecord<String, Object> record = new ProducerRecord<>(
+				topic, String.valueOf(new Random().nextInt(3) + 1), Integer.toHexString(new Random().nextInt(100))
+			);
+			producer.send(record);
+			producer.flush();
+			System.out.println("posted");
+			Thread.sleep(2000);
+//			producer.close();
+
+		}
+
+	}
+
+	public static Map<String, Object> producerConfigs(String kafkaServer) {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+		props.put(ProducerConfig.RETRIES_CONFIG, 0);
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+		props.put("request.required.acks", "1");
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return props;
+	}
+
+}
