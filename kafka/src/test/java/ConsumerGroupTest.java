e2383e4b47c92dcd77a3f3b9c4f5bd81a7b243d2
Elvis de Freitas <edigitalb@gmail.com>
2017-07-01 18:32:00 -0300

    MG-311 creating producer, consumer test with 2 groups and a key partition group

--- kafka/src/test/java/ConsumerGroupTest.java
+++ kafka/src/test/java/ConsumerGroupTest.java
@@ -1,11 +1,5 @@
 import com.mageddo.kafka.poc.ProducerExample;
 import com.mageddo.kafka.utils.KafkaEmbedded;
-import kafka.admin.AdminUtils;
-import kafka.admin.RackAwareMode;
-import kafka.utils.ZKStringSerializer$;
-import kafka.utils.ZkUtils;
-import org.I0Itec.zkclient.ZkClient;
-import org.I0Itec.zkclient.ZkConnection;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -13,15 +7,13 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.assertj.core.api.Assertions;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Properties;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -32,92 +24,68 @@ import java.util.concurrent.TimeUnit;
  */
 public class ConsumerGroupTest {
 
-	private static final String PING_TOPIC = "Ping2";
-	private static final String PING_GROUP_ID = "Ping.a1";
-	private static final int CONSUMERS_QUANTTITY = 20;
 	private static final Logger logger = LoggerFactory.getLogger(ConsumerGroupTest.class);
+	private static final String PING_TOPIC = "Ping";
+	private static final String PING_GROUP_ID_A = "Ping.a";
+	private static final String PING_GROUP_ID_B = "Ping.a";
+	private static final int EXPECTED_REGISTERS = 5;
+	private static final int CONSUMERS_QUANTTITY = 2;
 
-	private final Map<String, ConsumerRecord<String, String>> records = new TreeMap<>();
-	private final CountDownLatch countDownLatch = new CountDownLatch(5);
+	private final CountDownLatch aCountDownLatch = new CountDownLatch(EXPECTED_REGISTERS);
+	private final Map<String, ConsumerRecord<String, String>> aRecords = new HashMap<>();
 
-//	@ClassRule
-//	public static KafkaEmbedded kafkaEmbedded = new KafkaEmbedded(1, true, 1, PING_TOPIC);
-	public static KafkaEmbedded kafkaEmbedded = new KafkaEmbedded(1);
+	private final CountDownLatch bCountDownLatch = new CountDownLatch(EXPECTED_REGISTERS);
+	private final Map<String, ConsumerRecord<String, String>> bRecords = new HashMap<>();
 
+	@ClassRule
+	public static KafkaEmbedded kafkaEmbedded = new KafkaEmbedded(1, true, 2, PING_TOPIC);
 
 	@Test
 	public void testPostAndconsume() throws Exception {
 
-		kafkaEmbedded.before();
-
-		ZkClient client = new ZkClient(kafkaEmbedded.getZookeeperConnectionString(), 10000, 10000, ZKStringSerializer$.MODULE$);
-		ZkUtils zkUtils = new ZkUtils(client, new ZkConnection(kafkaEmbedded.getZookeeperConnectionString()), false);
-
-//        if(AdminUtils.topicExists(zkUtils, topic)){
-//            AdminUtils.deleteTopic(zkUtils, topic);
-//            AdminUtils.deleteAllConsumerGroupInfoForTopicInZK(zkUtils, topic);
-////            AdminUtils.deleteConsumerGroupInZK(zkUtils, groupId);
-//        }
-		try{
-			AdminUtils.createTopic(zkUtils, PING_TOPIC, 10, 1, new Properties(), RackAwareMode.Disabled$.MODULE$);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-
-		logger.info("status=posted");
 		final ExecutorService executorService = Executors.newFixedThreadPool(CONSUMERS_QUANTTITY);
 		for (int threadNum = 0; threadNum < CONSUMERS_QUANTTITY; threadNum++) {
-			executorService.submit(new SimpleConsumer(records, countDownLatch, kafkaEmbedded, PING_TOPIC, PING_GROUP_ID, String.valueOf(threadNum)));
+			executorService.submit(new SimpleConsumer(aRecords, aCountDownLatch, kafkaEmbedded, PING_TOPIC, PING_GROUP_ID_A, "a-" + threadNum));
+			executorService.submit(new SimpleConsumer(bRecords, aCountDownLatch, kafkaEmbedded, PING_TOPIC, PING_GROUP_ID_B, "b" + threadNum));
 		}
+		Thread.sleep(10_000);
 
 		final KafkaProducer<String, String> producer = new KafkaProducer<>(ProducerExample.config(kafkaEmbedded.getBrokersAsString()));
 		final String[][] msgs = {{"1", "0x1388"}, {"2", "0x2710"}, {"1", "0x4e20"}, {"2", "0x9c40"}, {"2", "0x13880"}, };
-//		for (int i = 0; i < msgs.length; i++) {
-		for(;;){
-//			final ProducerRecord<String, String> record = new ProducerRecord<>(PING_TOPIC, msgs[i][0], msgs[i][1]);
-//			producer.send(record);
-			producer.send(new ProducerRecord<>(PING_TOPIC, "x" ));
-			producer.flush();
+		for (int i = 0; i < msgs.length; i++) {
+			producer.send(new ProducerRecord<>(PING_TOPIC, msgs[i][0], msgs[i][1]));
 			logger.info("status=posted");
-			Thread.sleep(1000);
 
 		}
 
-//		countDownLatch.await(10, TimeUnit.SECONDS);
-//
-//		executorService.shutdown();
-//		executorService.awaitTermination(5, TimeUnit.SECONDS);
+		executorService.shutdown();
+		executorService.awaitTermination(EXPECTED_REGISTERS, TimeUnit.SECONDS);
+
+		aCountDownLatch.await(10, TimeUnit.SECONDS);
+		bCountDownLatch.await(10, TimeUnit.SECONDS);
+
+		Assertions.assertThat(this.aRecords.size()).isEqualTo(EXPECTED_REGISTERS);
+		Assertions.assertThat(this.aRecords.get("0x1388")).hasFieldOrPropertyWithValue("key", "1");
+
+		Assertions.assertThat(this.bRecords.size()).isEqualTo(EXPECTED_REGISTERS);
+		Assertions.assertThat(this.bRecords.get("0x1388")).hasFieldOrPropertyWithValue("key", "1");
+
 	}
 
 	private static Properties createConsumerConfig(String groupId, String kafkaServer, String zookeeper) {
-		Properties props = new Properties();
-		props.put("bootstrap.servers", kafkaServer);
-		props.put("zookeeper.connect", zookeeper);
-		props.put("group.id", groupId);
-		props.put("zookeeper.session.timeout.ms", "400");
-		props.put("zookeeper.sync.time.ms", "200");
-		props.put("auto.commit.interval.ms", "1000");
-
-		props.put("enable.auto.commit", true);
-		props.put("auto.commit.interval.ms", "1000");
-		props.put("session.timeout.ms", "30000");
-		props.put("key.deserializer", StringDeserializer.class);
-		props.put("value.deserializer", StringDeserializer.class);
-//		Properties props = new Properties();
-//		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
-//		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-//		props.put("zookeeper.connect", zookeeper);
-//		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-//		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-//		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-//
-//		props.put("key.deserializer", StringDeserializer.class);
-//		props.put("value.deserializer", StringDeserializer.class);
-
+		final Properties props = new Properties();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		return props;
 	}
 
 	static class SimpleConsumer implements Runnable {
+
 		private final Logger logger = LoggerFactory.getLogger(getClass());
 		private final Map<String, ConsumerRecord<String, String>> records;
 		private final CountDownLatch countDownLatch;
