d4414aa42ce69bf6d8eb63249fe696d8b06369a8
Elvis de Freitas <edigitalb@gmail.com>
2017-09-02 21:36:34 -0300

    generating random file

--- spark/src/main/java/com/mageddo/spark/students/vo/Student.java
+++ spark/src/main/java/com/mageddo/spark/students/vo/Student.java
@@ -6,6 +6,8 @@ public class Student implements Serializable {
 
 	public int id;
 	public String name;
+
+	public int schoolId;
 	public String schoolName;
 
 	public Student() {
@@ -18,10 +20,26 @@ public class Student implements Serializable {
 
 
 	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Student student = (Student) o;
+
+		return schoolName.equals(student.schoolName);
+	}
+
+	@Override
+	public int hashCode() {
+		return schoolName.hashCode();
+	}
+
+	@Override
 	public String toString() {
 		return "Student{" +
 			"id=" + id +
 			", name='" + name + '\'' +
+			", schoolId=" + schoolId +
 			", schoolName='" + schoolName + '\'' +
 			'}';
 	}
