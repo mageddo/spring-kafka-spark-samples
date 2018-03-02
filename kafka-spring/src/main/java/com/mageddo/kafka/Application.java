a14f72d13f8ed1f9b144e784425533f4b4d46471
Elvis de Freitas Souza <edigitalb@gmail.com>
2018-03-02 20:32:03 -0300

    spring kafka transactional

--- kafka-spring/src/main/java/com/mageddo/kafka/Application.java
+++ kafka-spring/src/main/java/com/mageddo/kafka/Application.java
@@ -1,42 +1,34 @@
 package com.mageddo.kafka;
 
 import com.mageddo.kafka.message.ConsumerDeclarer;
-import com.mageddo.kafka.message.RetryableKafkaListenerContainerFactory;
 import com.mageddo.kafka.message.TopicEnum;
 import com.mageddo.kafka.service.LineService;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.transaction.ChainedTransactionManager;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerEndpoint;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.AbstractMessageListenerContainer.AckMode;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.retry.backoff.ExponentialBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.transaction.KafkaTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.support.AbstractPlatformTransactionManager;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import javax.persistence.EntityManagerFactory;
 import java.util.concurrent.Executors;
 
 
@@ -46,6 +38,7 @@ import java.util.concurrent.Executors;
 @EnableAutoConfiguration
 
 @SpringBootApplication
+@EnableTransactionManagement
 @Configuration
 public class Application implements SchedulingConfigurer, InitializingBean {
 
@@ -56,9 +49,9 @@ public class Application implements SchedulingConfigurer, InitializingBean {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext app = SpringApplication.run(Application.class);
-//		LineService service = app.getBean(LineService.class);
-//		service.send();
-//		System.out.println("ok!");
+		LineService service = app.getBean(LineService.class);
+		service.send();
+		System.out.println("ok!");
 	}
 
 	@Override
@@ -69,5 +62,33 @@ public class Application implements SchedulingConfigurer, InitializingBean {
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		consumerDeclarer.declare(TopicEnum.values());
+	}
+
+	@Bean
+	public KafkaTransactionManager kafkaTransactionManager(ProducerFactory f) {
+		KafkaTransactionManager ktm = new KafkaTransactionManager(f);
+		ktm.setTransactionSynchronization(AbstractPlatformTransactionManager.SYNCHRONIZATION_ON_ACTUAL_TRANSACTION);
+		return ktm;
+	}
+
+	@Bean
+	@Primary
+	public DefaultKafkaProducerFactory x(KafkaProperties properties){
+		DefaultKafkaProducerFactory f = new DefaultKafkaProducerFactory<>(properties.buildProducerProperties());
+		f.setTransactionIdPrefix("myId");
+		return f;
+	}
+
+	// https://stackoverflow.com/questions/47354521/transaction-synchronization-in-spring-kafka
+	@Bean
+	@Primary
+	public JpaTransactionManager transactionManager(EntityManagerFactory em) {
+		return new JpaTransactionManager(em);
+	}
+
+	@Bean(name = "chainedTransactionManager")
+	public ChainedTransactionManager chainedTransactionManager(JpaTransactionManager jpaTransactionManager,
+																														 KafkaTransactionManager kafkaTransactionManager) {
+		return new ChainedTransactionManager(kafkaTransactionManager, jpaTransactionManager);
 	}
 }
--- kafka-spring/src/main/java/com/mageddo/kafka/service/LineServiceImpl.java
+++ kafka-spring/src/main/java/com/mageddo/kafka/service/LineServiceImpl.java
@@ -3,6 +3,7 @@ package com.mageddo.kafka.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -10,14 +11,17 @@ import java.time.LocalDateTime;
  * Created by elvis on 18/06/17.
  */
 @Service
+@Transactional
 public class LineServiceImpl implements LineService {
 
 	@Autowired
 	private KafkaTemplate kafkaTemplate;
 
 	@Override
+	@Transactional
 	public void send(){
 		kafkaTemplate.send("MyTopic", LocalDateTime.now().toString());
+		System.out.println("sent");
 	}
 
 }
